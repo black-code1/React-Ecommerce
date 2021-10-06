175c0f09923f42ddd4bca7e6506778544c02e467
black-code1 <legrandmahamat1@gmail.com>
2021-10-06 21:08:29 +0100

    Done with the Creation of Pure React Slider

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+	{
+		id: 1,
+		img: "https://images.unsplash.com/photo-1513094735237-8f2714d57c13?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHNob3BwaW5nJTIwYmFnfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+		title: "SUMMER SALE",
+		desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+		bg: "f5fafd",
+	},
+	{
+		id: 2,
+		img: "https://images.unsplash.com/photo-1559794872-d3bd697d45b8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=388&q=80",
+		title: "AUTUMN COLLECTION",
+		desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+		bg: "fcf1ed",
+	},
+	{
+		id: 3,
+		img: "https://images.unsplash.com/photo-1561203326-d65d921b5939?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80",
+		title: "LOUNGEWEAR LOVE",
+		desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+		bg: "fbf0f4",
+	},
+];
