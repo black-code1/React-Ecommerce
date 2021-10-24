e8ee98acd8dd5b435f5c0920da6988400cb13862
legrand-black <legrand@smartcodegroup.com>
2021-10-24 11:22:45 +0100

    Done with styled Components Hover & Animation

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,21 @@
 import { ShoppingCartOutlined, SearchOutlined, FavoriteBorderOutlined } from "@material-ui/icons"
 import styled from "styled-components"
 
+const Info = styled.div`
+  opacity:0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top:0;
+  left:0;
+  background-color:rgba(0,0,0,0.2);
+  z-index:3;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  transition: all 0.5s ease;
+  cursor:pointer;
+`
 const Container = styled.div`
   flex:1;
   margin:5px;
@@ -11,6 +26,10 @@ const Container = styled.div`
   justify-content: center;
   background-color: #f5fbfd;
   position: relative;
+
+  &:hover ${Info}{
+    opacity: 1;
+  }
 `
 
 const Circle = styled.div`
@@ -26,19 +45,6 @@ const Image = styled.img`
   z-index:2;
 `
 
-const Info = styled.div`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top:0;
-  left:0;
-  background-color:gray;
-  z-index:3;
-  display:flex;
-  align-items:center;
-  justify-content:center;
-`
-
 const Icon = styled.div`
   width:40px;
   height:40px;
@@ -48,6 +54,11 @@ const Icon = styled.div`
   align-items:center;
   justify-content: center;
   margin:10px;
+  transition: all 0.5s ease;
+  &:hover{
+    background-color: #e9f5f5;
+    transform:scale(1.1);
+  }
 `
 
 
